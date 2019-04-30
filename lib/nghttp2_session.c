730c8d6810ceb9094c1ff652786afe65b51db167
Stephen McQuistin <sm@smcquistin.uk>
2019-04-30 20:06:19 +0100

    Update nghttp2_session.c

--- lib/nghttp2_session.c
+++ lib/nghttp2_session.c
@@ -134,13 +134,13 @@ static int session_is_new_peer_stream_id(nghttp2_session *session,
 static int session_detect_idle_stream(nghttp2_session *session,
                                       int32_t stream_id) {
   /* Assume that stream object with stream_id does not exist */
-  if (nghttp2_session_is_my_stream_id(session, stream_id)) {
-    if (session->last_sent_stream_id < stream_id) {
-      printf("hmm 1\n");
-      return 1;
-    }
-    return 0;
-  }
+  //if (nghttp2_session_is_my_stream_id(session, stream_id)) {
+  //  if (session->last_sent_stream_id < stream_id) {
+  //    printf("hmm 1\n");
+  //    return 1;
+  //  }
+  //  return 0;
+  //}
   //if (session_is_new_peer_stream_id(session, stream_id)) {
   //  printf("hmm 2\n");
   //  return 1;
